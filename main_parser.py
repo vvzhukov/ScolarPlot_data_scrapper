b89f37e13e9215954b2a79b2703687bbea204427
vvzhukov <zhuko.v.italy@gmail.com>
2019-02-06 13:24:33 -0600

    Testing.

--- main_parser.py
+++ main_parser.py
@@ -6,8 +6,8 @@ from urllib.parse import quote
 from nameparser.parser import HumanName
 from time import sleep
 
-url1 = 'https://cm.utexas.edu/component/cobalt/category-items/1-directory/12-chemistry?Itemid=1252' # Lab link
-url2 = 'https://scholar.google.com/citations?view_op=search_authors&mauthors=' # Google scholar author search link
+url1 = 'https://www.cs.utexas.edu/faculty' # Dept link
+url2 = 'http://scholar.google.com/citations?view_op=search_authors&mauthors=' # Google scholar author search link
 url3 = 'https://scholar.google.ru/citations?user=' # Google scholar user link
 school_name = 'University of Texas at Austin'
 
@@ -24,7 +24,13 @@ filter = 'Google Online Clinical Seminar Science Program University Course Calen
          'Make Gift Work Works Occupationv Program Programs View Views Switch Switches /button Professor' \
          'Professorship Ecology Evolution Coordinator Forms Form Active Learn Learning Education' \
          'Educator Curriculum Active Form Forms Chair Biology Molecular Swap Employee Jr Telescope' \
-         'Future Table Tables Information Safety Informations Facility Facilities Nuclear Xray'
+         'Future Table Tables Information Safety Informations Facility Facilities Nuclear Xray' \
+         'Specialist Specialists Project Projects Art Arts Visual Visuals Crossing Crossings' \
+         'Sound Sounds Oh Film Critic Critics Republic Republics Producer Festival Festivals' \
+         'Chemistry Organic Inorganic Major Global Globals Award Awards Awarded Welcome' \
+         'Alumna Electron Microscopy Matter Matters Nanoscale PhD Lecturer Postdoc Postdocs ' \
+         'NYU Math Maths Architectural Architecture Earth Supervisor Supervisors Surface ' \
+         'Subserface Deep Climate Water'
 
 class AppURLOpener(FancyURLopener):
     version = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) ' \
@@ -116,22 +122,23 @@ def body_parser(in_url1, in_url2, in_url3, in_filter, in_sch_name):     # NEED2 
         else:
             if cont.count(user_tag) > 3:    # Multiply GS accounts validation; 3 user IDs ~ 1 user.
                 m += 1
-                print('More than one account found.')
+                ## print('More than one account found.')
                 if cont.find(in_sch_name) > 0:  # Searching for school name
                     k += 1
                     pos1 = cont.find(user_tag, cont.find(in_sch_name) - 350)    # Getting the first ID after schl name
                                                                                 # 350 symbols from the SchoolName
                                                                                 # hardcode - bad
-                    print('School found, account identified:')
+                    ## print('School found, account identified:')
                 else:
-                    print('School not found, picking first:')
+                    ## print('School not found, picking first:')
+                    names.pop(names.index(name))
             pos2 = cont.find('&amp;', pos1 + 1)
             gslink = in_url3 + cont[pos1 + 10:pos2] # Concat GS url + ID value
             print(last_first + "; " + gslink)
             output['last_first'] = gslink
             i += 1
 
-    print('-- Total people on lab page: ' + str(n))
+    print('-- Total people on dep page: ' + str(n))
     print('-- Total with G.S. profiles: ' + str(i) + ' (multi acc: ' + str(m) + '; school identified: ' + str(k) + ')')
 
     return output
@@ -140,3 +147,58 @@ def body_parser(in_url1, in_url2, in_url3, in_filter, in_sch_name):     # NEED2 
 # MAIN
 
 body_parser(url1, url2, url3, filter, school_name)
+
+exit()
+'''
+No more than 3800 per hour! Limitation from Google. Thanks to Mohammed's emperical testing.
+Test Results
+1. school_name = 'University of Texas at Austin'
+
+1.1. Physics: https://ph.utexas.edu/component/cobalt/category-items/1-directory/18-physics?Itemid=1264
+-- Total people on dep page: 54
+-- Total with G.S. profiles: 19 (multi acc: 8; school identified: 1)
+In ScholarPlot: 12 profiles
+
+1.2. Mathematics: https://www.ma.utexas.edu/component/cobalt/category-items/1-directory/15-mathematics?Itemid=1334
+-- Total people on dep page: 63
+-- Total with G.S. profiles: 19 (multi acc: 12; school identified: 2)
+In ScholarPlot: 20 profiles
+
+1.3. Neuroscience: https://neuroscience.utexas.edu/component/cobalt/category-items/1-directory/17-neuroscience?Itemid=1225
+-- Total people on dep page: 59
+-- Total with G.S. profiles: 23 (multi acc: 8; school identified: 1)
+In ScholarPlot: 13 profiles
+
+2. school_name = 'New York University'
+2.1 Biology: http://as.nyu.edu/biology/people/faculty.html
+-- Total people on dep page: 85
+-- Total with G.S. profiles: 43 (multi acc: 12; school identified: 4)
+In ScholarPlot: 34
+
+2.2 Chemistry: http://as.nyu.edu/chemistry/people/faculty.html
+-- Total people on dep page: 96
+-- Total with G.S. profiles: 30 (multi acc: 16; school identified: 3)
+In ScholarPlot: 19
+
+2.3 Mathematics: https://www.math.nyu.edu/dynamic/people/faculty/
+
+
+In ScholarPlot: 30
+
+3. school_name = 'Columbia University'
+3.1 Film: https://arts.columbia.edu/film/faculty
+
+
+In ScholarPlot: 0 profiles
+
+3.2 Visual Arts: https://arts.columbia.edu/visual-arts/faculty
+
+In ScholarPlot: 0 profiles
+
+
+3.3 Writing: https://arts.columbia.edu/writing/faculty
+
+
+In ScholarPlot: 0 profiles
+
+'''
